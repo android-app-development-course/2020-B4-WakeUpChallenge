056647e461b35cf8af3c5f0c5a45cd28c7308640
crazy_fz <2454519050@qq.com>
2020-11-23 10:54:28 +0800

    增加分工结构

---
+++ code/app/src/main/java/com/example/wakeupchallenge/AlarmGameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wakeupchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_game)
+    }
+}
