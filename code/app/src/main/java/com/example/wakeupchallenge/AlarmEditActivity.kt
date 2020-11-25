a92ad1d29306ed98a088de3f18866e02fcb0cc8b
crazy_fz <2454519050@qq.com>
2020-11-25 16:33:28 +0800

    添加对传入数据的处理

--- code/app/src/main/java/com/example/wakeupchallenge/AlarmEditActivity.kt
+++ code/app/src/main/java/com/example/wakeupchallenge/AlarmEditActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.Window
 import android.widget.NumberPicker
 import kotlinx.android.synthetic.main.activity_alarm_edit.*
@@ -15,24 +16,62 @@ class AlarmEditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_alarm_edit)
-        val time=Calendar.getInstance()
-        val g=time.get(Calendar.AM_PM)
-        var h=time.get(Calendar.HOUR)
-        if (g==Calendar.PM)
-            h+=12
-        val m=time.get(Calendar.MINUTE)
+        val inBun=intent.extras
+
+        var h:Int
+        var m:Int
+        var name="闹钟"
+        var music="铃声"
+
+        if(inBun?.getString("ACTION")=="CREATE"){
+            nameEdit.setText(name.toCharArray(),0,name.length)
+            musicName.setText(music.toCharArray(),0,music.length)
+            delBtn.visibility= View.INVISIBLE
+            val time=Calendar.getInstance()
+            val g=time.get(Calendar.AM_PM)
+            h=time.get(Calendar.HOUR)
+            if (g==Calendar.PM)
+                h+=12
+            m=time.get(Calendar.MINUTE)
+            repeatSwitch.isChecked=false
+        }
+        else{
+            h=inBun!!.getInt("hour")
+            m=inBun.getInt("min")
+            name = inBun.getString("name")!!
+            nameEdit.setText(name.toCharArray(),0,name.length)
+            repeatSwitch.isChecked=inBun.getBoolean("repeat")
+        }
+
         initTimePicker(h,m)
 //        ll.setOnClickListener{
 //            Log.d("bbb", "aaa")
 //
 //        }
+
+        val ret=Intent()
+        val retBun=Bundle()
+
         cancelBtn.setOnClickListener{
-            val ret=Intent()
             setResult(Activity.RESULT_CANCELED,ret)
             finish()
         }
         okBtn.setOnClickListener{
+            retBun.putInt("hour",hourPick.value)
+            retBun.putInt("min",minutePick.value)
+            retBun.putBoolean("repeat",repeatSwitch.isChecked)
+            retBun.putString("name",nameEdit.text.toString())
+            retBun.putString("IS_DEL","FALSE")
 
+            ret.putExtras(retBun)
+            setResult(Activity.RESULT_OK,ret)
+            finish()
+        }
+        delBtn.setOnClickListener{
+            retBun.putString("IS_DEL","TRUE")
+
+            ret.putExtras(retBun)
+            setResult(Activity.RESULT_OK,ret)
             finish()
         }
     }
--- code/app/src/main/java/com/example/wakeupchallenge/MainActivity.kt
+++ code/app/src/main/java/com/example/wakeupchallenge/MainActivity.kt
@@ -1,14 +1,53 @@
 package com.example.wakeupchallenge
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        testAlarmEdit()
+    }
+
+    private fun testAlarmEdit(){
+        // 测试用
+        val bundle=Bundle()
+        bundle.putString("ACTION","CREATE")
+        bundle.putString("name","test")
+        bundle.putInt("hour",12)
+        bundle.putInt("min",13)
+        bundle.putString("music","testMusic")
+        bundle.putBoolean("repeat",false)
+
         val intent=Intent(this,AlarmEditActivity::class.java)
-        startActivity(intent)
+        intent.putExtras(bundle)
+        startActivityForResult(intent,1)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            1->if (resultCode== Activity.RESULT_OK){
+                val bundle=data?.extras
+                if(bundle?.getString("IS_DEL")=="TRUE"){
+                    Log.d("ActivityResult", "del")
+                    return
+                }
+                else{
+                    val name=bundle?.getString("name")
+                    val h=bundle?.getInt("hour")
+                    val m=bundle?.getInt("min")
+                    val r=bundle?.getBoolean("repeat")
+                    Log.d("ActivityResult", name!!)
+                    Log.d("ActivityResult",h.toString())
+                    Log.d("ActivityResult",m.toString())
+                    Log.d("ActivityResult", r.toString())
+
+                }
+            }
+        }
     }
 }
